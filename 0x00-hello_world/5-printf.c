c9b459c49a3bac4354a73fe64d9ed5f0040823a5
Onah Godwin <lil_onah@icloud.com>
2023-09-11 19:11:26 -0700

    created a file that prints an output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A simple C program that prints out an output using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
