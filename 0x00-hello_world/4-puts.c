7c42c8380732f58bc30f6694b90f754b6bc05fdc
Onah Godwin <lil_onah@icloud.com>
2023-09-11 18:02:19 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,13 @@
 
 /**
  * main - Entry point
- *
- * Author: Sammykingx
- *
+ * Author: lil-onah
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char string[70] = "\"Programming is like building a multilingual puzzle\"";
-	
+
 	puts(string);
 
 	/* Tried printing it directly */
