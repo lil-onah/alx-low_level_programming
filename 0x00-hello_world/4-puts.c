9fdc94256ca8b403c8aa7967a1ecb2543a222b90
Onah Godwin <lil_onah@icloud.com>
2023-09-11 18:50:50 -0700

    Another correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char string[70] = "\"Programming is like building a multilingual puzzle\";
+	char string[70] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
 	return (0);
