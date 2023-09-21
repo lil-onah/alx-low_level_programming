de64788ca8c1d92df8b993895cac63acb39bef91
Onah Godwin <129755030+lil-onah@users.noreply.github.com>
2023-09-21 08:53:34 -1200

    Create 5-print_number.c

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: onah
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int sammy;
+
+	for (sammy = 0; sammy < 10; sammy++)
+	{
+		printf("%d", sammy);
+	}
+	printf("\n");
+	return (0);
+}
