b0832647ac201bd4344b4d58c6559a6b1c9ff2fc
Onah GOdwin <lil_onah@icloud.com>
2024-01-01 11:42:06 -0800

    made few corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,20 +8,21 @@
  */
 int main(void)
 {
-        int a, b;
+int a, b;
 
-        for (a = 48; a <=57; a++)
-        {
-                for (b = 49; b <= 57; b++)
-                {
-                        if ( a != b && b > a)
-                        {
-                                putchar(a);
-                                putchar(b);
-                                putchar(',');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+for (a = 48; a <= 57; a++)
+{
+for (b = 49; b <= 57; b++)
+{
+if (a != b && b > a)
+{
+putchar(a);
+putchar(b);
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
