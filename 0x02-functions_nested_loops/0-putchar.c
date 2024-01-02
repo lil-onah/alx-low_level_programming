b55ea5f2c4c81f0022064c747060b4ce9d6d47fd
Onah GOdwin <lil_onah@icloud.com>
2024-01-02 14:20:54 -0800

    firt task done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** main - Entry point
+ * Description: a code that prints putchar
+ * Return value - it should return an int 
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
