ca74eb4137c9b4bda7775c181b4fbd4980daa4f8
Onah GOdwin <lil_onah@icloud.com>
2024-01-04 17:41:11 -0800

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: returns an int value
+ * Return: return Always (success)
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+	return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
