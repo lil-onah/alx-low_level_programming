8f9dfec6b4af3ba2d9b97df00166f0d415e3cac5
Onah GOdwin <lil_onah@icloud.com>
2024-01-05 01:55:52 -0800

    done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: return Always (success)
+ */
+
+void times_table(void)
+{
+	int o, g, result;
+
+	for (o = 0; o <= 9; o++)
+	{
+		for (g = 0; g <= 9; g++)
+		{
+			result = o * g;
+			if (g == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+			{
+				_putchar(result / 10);
+				_putchar(result % 10);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
