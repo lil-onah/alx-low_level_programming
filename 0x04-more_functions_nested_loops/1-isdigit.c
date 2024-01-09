2ac7b31d8e7e3ad4052f9fd61322bea709bcef2c
Onah Godwin <129755030+lil-onah@users.noreply.github.com>
2024-01-09 06:26:03 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,12 +8,12 @@
 
 int _isdigit(int c)
 {
-	if ((c > 'a' && c < 'Z') || (c > 'A' && c < 'Z'))
+	if (c >= '0' && c <= '9')
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
