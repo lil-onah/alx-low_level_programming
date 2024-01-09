2ebdd5a042319b15f6bef1fc0bc6fdca09206a04
Onah GOdwin <lil_onah@icloud.com>
2024-01-08 20:28:14 -0800

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - prints multiple of two numbers
+ * @a: first number that multiplies with another digit
+ * @b: multiplies with first digits
+ * Return: return Always (success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
